1cd72146fffc0461bd5f909323c5bc095f98aeb1
Ekaluk52003 <ekaluk.p@gmail.com>
2021-08-03 22:50:50 +0700

    Build server and add .ebextensions

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const express = require('express');
+import path from 'path';
+import express from 'express';
 const PORT = process.env.HTTP_PORT || 4001;
 const app = express();
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.get('/', (req, res) => {
-    res.send('just gonna send it');
+  res.send('just gonna send it');
+});
+app.get('/flower', (req, res) => {
+  res.json({
+    name: 'Dandelion',
+    colour: 'Blue-ish'
   });
-  app.get('/flower', (req, res) => {
-    res.json({
-      name: 'Dandelion',
-      colour: 'Blue-ish'
-    });
-  });
-  app.listen(PORT, () => {
-    console.log(`Server listening at port ${PORT}.`);
-  });
+});
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}.`);
+});
