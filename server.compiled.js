35cc364a43873e47ec06598eeacd284d53b622d7
Ekaluk52003 <ekaluk.p@gmail.com>
2021-08-04 19:42:41 +0700

    update profile

--- server.compiled.js
+++ server.compiled.js
@@ -7,15 +7,16 @@ var _express = _interopRequireDefault(require("express"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var _process$env$PORT = process.env.PORT,
-    PORT = _process$env$PORT === void 0 ? 3000 : _process$env$PORT;
+    PORT = _process$env$PORT === void 0 ? 3000 : _process$env$PORT; // const PORT = process.env || 3000;
+
 var app = (0, _express["default"])();
 app.use(_express["default"]["static"](_path["default"].join(__dirname, 'client', 'build')));
 app.get('/', function (req, res) {
-  res.send('just gonna send it');
+  res.send('just');
 });
 app.get('/flower', function (req, res) {
   res.json({
-    name: 'Dandelion',
+    name: 'Dandelion New api',
     colour: 'Blue-ish'
   });
 });
